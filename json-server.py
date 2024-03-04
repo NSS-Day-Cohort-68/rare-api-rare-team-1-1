2600673716ecffae5c7db4ff1d13d7a906c3f5ce
Claire Hanson <clairecatohanson@gmail.com>
2024-03-04 14:53:37 -0600

    implement create_user and server file structure

---
+++ json-server.py
@@ -0,0 +1,40 @@
+import json
+from http.server import HTTPServer
+from handler import HandleRequests, status
+
+from views import login_user, create_user
+
+
+class JSONServer(HandleRequests):
+    def do_GET(self):
+        pass
+
+    def do_POST(self):
+        url = self.parse_url(self.path)
+
+        content_len = int(self.headers.get("content-length", 0))
+        request_body = self.rfile.read(content_len)
+        request_body = json.loads(request_body)
+
+        if url["requested_resource"] == "users":
+            test = create_user(request_body)
+            return self.response(
+                "endpoint is working",
+                status.HTTP_200_SUCCESS.value,
+            )
+
+    def do_PUT(self):
+        pass
+
+    def do_DELETE(self):
+        pass
+
+
+def main():
+    host = ""
+    port = 8000
+    HTTPServer((host, port), JSONServer).serve_forever()
+
+
+if __name__ == "__main__":
+    main()
---
+++ views/__init__.py
@@ -0,0 +1,2 @@
+from .user import login_user
+from .user import create_user
