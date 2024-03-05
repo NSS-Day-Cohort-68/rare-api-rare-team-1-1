f4fad726c4281825cd2d1adbf26bd220700aeb67
Emily Mai <emily.vy.mai@gmail.com>
2024-03-05 13:18:07 -0600

    tags & comments post method

--- json-server.py
+++ json-server.py
@@ -3,6 +3,7 @@ from http.server import HTTPServer
 from handler import HandleRequests, status
 
 from views import login_user, create_user
+from views import create_comment
 
 
 class JSONServer(HandleRequests):
@@ -21,6 +22,28 @@ class JSONServer(HandleRequests):
             return self.response(
                 "endpoint is working",
                 status.HTTP_200_SUCCESS.value,
+            )
+
+        if url["requested_resource"] == "comments":
+            successfully_created = create_comment(request_body)
+            if successfully_created:
+                return self.response(
+                    "Successfully created", status.HTTP_201_SUCCESS_CREATED.value
+                )
+
+            return self.response(
+                "Invalid data", status.HTTP_400_CLIENT_ERROR_BAD_REQUEST_DATA.value
+            )
+
+        if url["requested_resource"] == "tags":
+            successfully_created = create_comment(request_body)
+            if successfully_created:
+                return self.response(
+                    "Successfully created", status.HTTP_201_SUCCESS_CREATED.value
+                )
+
+            return self.response(
+                "Invalid data", status.HTTP_400_CLIENT_ERROR_BAD_REQUEST_DATA.value
             )
 
     def do_PUT(self):
--- views/__init__.py
+++ views/__init__.py
@@ -1,2 +1,4 @@
 from .user import login_user
 from .user import create_user
+from .comment import create_comment
+from .tag import create_tag
