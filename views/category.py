d3c4869d14d838120b16fa3fc92287f61b4dc134
Luke Williams <whatoftoday2002@gmail.com>
2024-03-05 10:43:25 -0600

    added post_category

--- views/category.py
+++ views/category.py
@@ -1,2 +1,17 @@
 import sqlite3
 import json
+
+
+def post_category(category_data):
+    with sqlite3.connect("./db.sqlite3") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute(
+            """
+            INSERT INTO Category (label)
+            VALUES (?)
+            """,
+            (category_data['label'])
+            )
+
+    return True if db_cursor.rowcount > 0 else False
