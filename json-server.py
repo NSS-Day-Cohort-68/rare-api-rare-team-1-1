1e2ff051d8120a726431047c09c42d6d191ea845
Claire Hanson <clairecatohanson@gmail.com>
2024-03-05 14:33:53 -0600

    api create_user functionality working

--- json-server.py
+++ json-server.py
@@ -1,4 +1,5 @@
 import json
+from json.decoder import JSONDecodeError
 from http.server import HTTPServer
 from handler import HandleRequests, status
 
@@ -12,16 +13,42 @@ class JSONServer(HandleRequests):
     def do_POST(self):
         url = self.parse_url(self.path)
 
-        content_len = int(self.headers.get("content-length", 0))
-        request_body = self.rfile.read(content_len)
-        request_body = json.loads(request_body)
+        try:
+            content_len = int(self.headers.get("content-length", 0))
+            request_body = self.rfile.read(content_len)
+            request_body = json.loads(request_body)
+        except JSONDecodeError:
+            return self.response(
+                "Error -- No user information was provided.",
+                status.HTTP_400_CLIENT_ERROR_BAD_REQUEST_DATA.value,
+            )
 
         if url["requested_resource"] == "users":
-            test = create_user(request_body)
-            return self.response(
-                "endpoint is working",
-                status.HTTP_200_SUCCESS.value,
-            )
+            try:
+                expected_user_keys = [
+                    "first_name",
+                    "last_name",
+                    "username",
+                    "email",
+                    "bio",
+                    "password",
+                    "profile_image_url",
+                    "created_on",
+                    "active",
+                ]
+                for key in expected_user_keys:
+                    value = request_body[key]
+            except KeyError:
+                return self.response(
+                    "Incomplete user information. Please provide values for first_name, last_name, username, and email.",
+                    status.HTTP_400_CLIENT_ERROR_BAD_REQUEST_DATA.value,
+                )
+
+            token = create_user(request_body)
+            return self.response(token, status.HTTP_201_SUCCESS_CREATED.value)
+        return self.response(
+            "Unexpected error occurred", status.HTTP_500_SERVER_ERROR.value
+        )
 
     def do_PUT(self):
         pass
--- views/user.py
+++ views/user.py
@@ -13,7 +13,7 @@ def login_user(user):
         json string: If the user was found will return valid boolean of True and the user's id as the token
                      If the user was not found will return valid boolean False
     """
-    with sqlite3.connect("./db.sqlite3") as conn:
+    with sqlite3.connect("./rare-api-rare-team-1-1/db.sqlite3") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
@@ -46,26 +46,26 @@ def create_user(user):
     Returns:
         json string: Contains the token of the newly created user
     """
-    with sqlite3.connect("./rare-api-rare-team-1/db.sqlite3") as conn:
+    with sqlite3.connect("./rare-api-rare-team-1-1/db.sqlite3") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
         db_cursor.execute(
             """
-        INSERT INTO Users (first_name, last_name, username, email, password, bio, profile_image_url, created_on, active) values (?, ?, ?, ?, ?, ?, ?, ?, 1)
+        INSERT INTO Users (first_name, last_name, bio, email, username, password, profile_image_url, created_on, active) values (?, ?, ?, ?, ?, ?, ?, ?, 1)
         """,
             (
                 user["first_name"],
                 user["last_name"],
-                user["username"],
-                user["email"],
-                user["password"],
                 user["bio"],
-                "test url",
+                user["email"],
+                user["username"],
+                user["password"],
+                user["profile_image_url"],
                 datetime.now(),
             ),
         )
 
-        id = db_cursor.lastrowid
+        row_id = db_cursor.lastrowid
 
-        return json.dumps({"token": id, "valid": True})
+        return json.dumps({"token": row_id, "valid": True})
